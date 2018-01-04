ac5ba6e7729d1984a9404a883358936021895d03
Paolo Frias <pbfrias17@Paolos-MacBook-Pro.local>
2018-01-04 18:17:28 -0500

    better routing maybe

--- routers/roomRouter.js
+++ routers/roomRouter.js
@@ -8,66 +8,107 @@ import DBHelpers from '../db/helpers';
 const roomRouter = express.Router();
 
 roomRouter.route('/open').post((req, res) => {
-    const { id } = req.headers;
-    const newRoom = { id };
+    const roomId = req.headers['room-id'];
+    const newRoom = { id: roomId };
     const token = auth.createToken(newRoom);
     newRoom.token = token;
-    console.log(newRoom);
+
     Room.create(newRoom)
         .then((data) => {
-            res.status(200).send('ok');
-        }).catch((err) => {
-            res.status(500).send({ error: err });
+            res.status(200).send({ success: true, message: 'Room has been opened.' });
+        }).catch((error) => {
+            res.status(500).send({ error });
         });
 });
 
 roomRouter.route('/close').post((req, res) => {
     // Verify the request is coming from an authenticated source.
     const token = req.headers['x-access-token'];
-    const id = req.headers['id'];
+    const roomId = req.headers['room-id'];
 
     auth.verifyToken(token)
         .then((decoded) => {
-            if (id === decoded.id) {
-                Room.remove({ id })
-                .then((result) => {
-                    res.status(200).send({ auth: true });
-                }).catch((err) => {
-                    res.status(500).send({ auth: true, error: 'Could not close room.' });
-                });
+            if (roomId === decoded.id) {
+                // Remove all players from the room.
+
+                // Close the room.
+                Room.remove({ id: roomId })
+                    .then((result) => {
+                        if (result.n === 0) {
+                            res.status(200).send({ success: false, message: 'That room does not exist.' });
+                        } else {
+                            res.status(200).send({ success: true, message: 'Room has been closed.' });                        
+                        }
+                    }).catch((err) => {
+                        res.status(500).send({ auth: true, error: 'Could not close room.' });
+                    });
             } else {
-                res.status(400).send({ auth: false, error: 'Not authorized to close this room' });
+                res.status(400).send({ auth: false, error: 'Not authorized to close this room.' });
             }
-        }).catch((err) => {
-            res.status(401).send({ auth: false, error: 'Access token could not be authenticated.' });
+        }).catch((error) => {
+            res.status(401).send({ auth: false, error });
         });
 });
 
 roomRouter.route('/playerEnter').post((req, res) => {
-    const { id } = req.headers;
-
-    // Verify the room has capacity for a new player,
-
+    const roomId = req.headers['room-id'];
+    // Verify the room has capacity for a new player.
+    
 
     // Create new user and add them to the specified room.
     const newPlayer = { id: uuidv4(), name: '' };
     const token = auth.createToken(newPlayer);
     newPlayer.token = token;
 
-    Room.findOneAndUpdate({ id }, { $push: { players: newPlayer } }, (err, doc, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Player could not enter room');
-        } else {
-            res.status(200).send(result);
-        }
-    }); 
+    const conditions = { id: roomId };
+    const query = { $push: { players: newPlayer } };
+    const options = { new: true };
+    Room.findOneAndUpdate(conditions, query, options)
+        .then((result) => {
+            if (result) {
+                res.status(200).send({ success: true, message: 'Player entered the room.' });
+            } else {
+                res.status(200).send({ success: false, message: 'That room could not be found.' });
+            }
+        }).catch((error) => {
+            res.status(500).send({ error });
+        }); 
 });
 
 roomRouter.route('/playerLeave').post((req, res) => {
-    // Verify player is in target room, then remove them.
+    const token = req.headers['x-access-token'];
+    let roomId = req.headers['room-id'];
+    let playerId = req.headers['player-id'];
 
-    // Close room if no players remain.
+    // Verify the request is coming from an authenticated source.
+    auth.verifyToken(token)
+        .then((decoded) => {
+            // Headers are dependent on source of request (Room or Player).
+            if (!playerId)
+                playerId = decoded.id;
+            else
+                roomId = decoded.id;
+
+            // Verify player is in target room, then remove them.
+            const conditions = { id: roomId };
+            const query = { $pull: { players: { id: playerId } } };
+            const options = { new: true };
+            Room.findOneAndUpdate(conditions, query, options)
+                .then((result) => {
+                    console.log(result); 
+                    res.status(200).send({ success: true, message: 'Player left the room.' });
+                    
+                    // Close room if no players remain.
+                    console.log(result)
+                    if (result && result.players.length === 0) {
+                        Room.remove({ id: roomId }).exec();
+                    }
+                }).catch((error) => {
+                    res.status(500).send({ error });
+                })
+        }).catch((error) => {
+            res.status(400).send({ auth: false, error });
+        })
 });
 
 export default roomRouter;
